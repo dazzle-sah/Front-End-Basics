7eafd532c5d98b7003e8c3f8429ceb41c4331a92
drishtisah <drishu.sah@gmail.com>
2022-07-15 18:58:52 +0530

    js commit

--- Calculator/main2.js
+++ Calculator/main2.js
@@ -4,3 +4,15 @@ let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=> {
    
     button.addEventListener('click' , (e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'C'){
+            string="";
+            document.querySelector('input').value = string;
+        }
+
+    })
+   
+})
