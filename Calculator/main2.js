813bb084aaff7d6da876111a5ce40a356db45582
drishtisah <drishu.sah@gmail.com>
2022-07-15 18:59:28 +0530

    condition

--- Calculator/main2.js
+++ Calculator/main2.js
@@ -4,6 +4,7 @@ let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=> {
    
     button.addEventListener('click' , (e)=>{
+
         if(e.target.innerHTML == '='){
             string = eval(string);
             document.querySelector('input').value = string;
@@ -12,6 +13,18 @@ Array.from(buttons).forEach((button)=> {
             string="";
             document.querySelector('input').value = string;
         }
+
+        else if (e.target.innerHTML == 'x'){
+            string = string.substring(0,string.length-1);
+            document.querySelector('input').value = string;
+        }
+
+        else if(e.target.innerHTML == undefined){
+            throw new console.error('ERROR');
+            document.querySelector('input').value = 'error';
+
+        }
+
 
     })
    
