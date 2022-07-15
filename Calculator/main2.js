6b5aff584362d919b12c49aa578e59dea776ec25
drishtisah <drishu.sah@gmail.com>
2022-07-15 18:59:58 +0530

    final commit

--- Calculator/main2.js
+++ Calculator/main2.js
@@ -25,6 +25,11 @@ Array.from(buttons).forEach((button)=> {
 
         }
 
+        else{
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+             }
 
     })
    
