0c216c00a4563b30f2bd54b85bce3824e608950d
drishtisah <drishu.sah@gmail.com>
2022-07-20 17:20:41 +0530

    collision

--- Snake/snakeee.js
+++ Snake/snakeee.js
@@ -18,3 +18,15 @@ function main(ctime){
  gameEngine();
 }
 
+function isCollide(snake) {
+    for (let i = 1; i < snakeArr.length; i++) {
+        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+            return true;
+        }
+    }
+    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
+        return true;
+    }
+        
+    return false;
+}
